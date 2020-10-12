2d88ace6bdc7035ca8d16c3b466c8ed33b7a203b
Xwg <loloxwg@outlook.com>
2020-10-12 14:39:12 +0800

    Update P5705 【深基2.例7】数字反转.cc

--- 顺序表/P5705 【深基2.例7】数字反转.cc
+++ 顺序表/P5705 【深基2.例7】数字反转.cc
@@ -8,3 +8,16 @@ int main()
     cout<<a;              //输出
     return 0;             //养成好习惯
 }
+
+
+#include<bits/stdc++.h>
+using namespace std;
+string a;
+signed main()
+{
+	cin>>a;
+	int len=a.size();//读取长度
+	for(int i=len-1;i>=0;i--)//反转
+	cout<<a[i];
+	return 0;
+}
