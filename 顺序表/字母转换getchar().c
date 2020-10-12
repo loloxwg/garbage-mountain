4b17105a9775031d832ac350b993baa515858124
Xwg <loloxwg@outlook.com>
2020-10-12 14:29:00 +0800

    Update 字母转换getchar().c

--- 顺序表/字母转换getchar().c
+++ 顺序表/字母转换getchar().c
@@ -8,3 +8,14 @@ int main(){
     ch=ch-32;
     printf("%c %d %d",ch,ch,sizeof(ch));
 }
+
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main() {
+	char a; //定义一个字符
+	cin>>a; //输入
+	cout<<(char)(a-('a'-'A'));
+   //由于字符存储的是ASCII码，所以先用一个字符小写-大写做例子，再用a-这个差，强制类型转换，得出此值
+	return 0;
+}
