01967568c575c21fd07cbd89ef6fc695dc0c948a
Xwg <loloxwg@outlook.com>
2020-10-12 12:40:10 +0800

    Create P5705 【深基2.例7】数字反转.cpp

---
+++ 顺序表/P5705 【深基2.例7】数字反转.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>   //文件头
+using namespace std;
+string a;                 //定义字符串
+int main() 
+{
+    cin>>a;               //输入
+    reverse(a.begin(),a.end());  //反转
+    cout<<a;              //输出
+    return 0;             //养成好习惯
+}
