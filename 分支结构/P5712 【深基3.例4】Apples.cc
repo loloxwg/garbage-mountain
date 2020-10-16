1aa8c81dc4c9a28d6a2b97611011ff822a989bdc
Xwg <loloxwg@outlook.com>
2020-10-17 01:46:40 +0800

    Create P5712 【深基3.例4】Apples.cc

---
+++ 分支结构/P5712 【深基3.例4】Apples.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    if (a<=1)
+    {
+        printf("Today, I ate %d apple.",a);
+    }else if (a>1)
+    {
+        printf("Today, I ate %d apples.",a);
+    }
+    
+    
+}
